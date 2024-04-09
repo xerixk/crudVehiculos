6660b445d6ff0d879e940736357c1e086f91cc91
erick <eric.suarez@ara-tech.es>
2024-04-09 13:00:01 +0200

    crud php vehiculos

--- model/Usuario.php
+++ model/Usuario.php
@@ -23,17 +23,20 @@ class Usuario{
         return $datos;
     }
     public function crear($nombre,$dni,$vehiculo_id){
-       
-        
-        $sql="INSERT INTO usuarios (nombre,dni,vehiculo_id) VALUES ('$nombre','$dni',$vehiculo_id)";
-        $stmn=$this ->conn->prepare($sql);
-       if( $stmn->execute()){
-        return true;
-       }else{
+       if($this->leerDNI($dni)){
+        echo "Ya existe un usuario con dni = $dni";
         return false;
+       }else{
+            $sql="INSERT INTO usuarios (nombre,dni,vehiculo_id) VALUES ('$nombre','$dni',$vehiculo_id)";
+            $stmn=$this ->conn->prepare($sql);
+        if( $stmn->execute()){
+            echo "creado correctamente";
+            return true;
+        }else{
+            echo "error al crear";
+            return false;
+        }
        }
-
-       
     }
     public function leer($id){
         $sql="SELECT * FROM usuarios where id=$id";
@@ -42,20 +45,33 @@ class Usuario{
         $datos= $stmn->fetchAll(PDO::FETCH_ASSOC);
         return $datos;
     }
-    public function eliminar($id){
-        $sql="DELETE * FROM usuarios where id=$id";
+    public function leerDNI($dni){
+        $sql="SELECT * FROM usuarios where dni='$dni'";
         $stmn=$this ->conn->prepare($sql);
-        if( $stmn->execute()){
-            echo "eliminado Correctamente";
-         return true;
+        $stmn->execute();
+        $datos= $stmn->fetchAll(PDO::FETCH_ASSOC);
+        return $datos;
+    }
+    
+    public function eliminar($id){
+        if($this->leer($id)){
+            $sql = "DELETE FROM usuarios where id = $id";
+            $stmt = $this->conn->prepare($sql);
+            if($stmt->execute()){
+                echo "Eliminado correctamente";
+                return true;
+            }else{
+                echo "No Eliminado";
+                return false;
+            }
         }else{
-            echo "Error al eliminar";
-         return false;
+            echo "No existe un usuario con id = $id";
+            return false;
         }
     }
     public function actualizar($nombre,$dni,$vehiculo_id,$id){
 
-        
+        if($this->leer($id)){
         $sql="UPDATE usuarios set nombre='$nombre',dni='$dni',vehiculo_id=$vehiculo_id where id=$id";
         $stmn=$this ->conn->prepare($sql);
         if( $stmn->execute()){
@@ -65,14 +81,18 @@ class Usuario{
             echo "Error al actualizar";
          return false;
         }
+        }else{
+            echo "No existe un usuario con id = $id";
+            return false;
+        }
     }
 }
 
 
-$dni="1234467F";
-$nombre="Erickkk";
-$vehiculo_id=3;
+$dni="1234468F";
+$nombre="Rober";
+$vehiculo_id=2;
 
 $u= new Usuario();
-var_dump($u->actualizar($dni,$nombre,$vehiculo_id,6));
+var_dump($u->crear($nombre,$dni,$vehiculo_id));
 ?>
--- model/vehiculo.php
+++ model/vehiculo.php
@@ -1,3 +1,99 @@
 <?php
 
+require_once "Conexion.php";
+
+class Usuario{
+    private $conn;
+    public $id;
+    public $marca;
+    public $color;
+    public $kilometros;
+
+    public function __construct(){
+        $db= new Conexion();
+        $this->conn =$db->getConexion();
+
+    }
+
+    public function listar(){
+        $sql="SELECT * FROM vehiculos";
+        $stmn=$this ->conn->prepare($sql);
+        $stmn->execute();
+        $datos= $stmn->fetchAll(PDO::FETCH_ASSOC);
+        return $datos;
+    }
+    public function leer($id){
+        $sql="SELECT * FROM vehiculos where id=$id";
+        $stmn=$this ->conn->prepare($sql);
+        $stmn->execute();
+        $datos= $stmn->fetchAll(PDO::FETCH_ASSOC);
+        return $datos;
+    }
+    public function leerMatricula($matricula){
+        $sql="SELECT * FROM vehiculos where matricula='$matricula'";
+        $stmn=$this ->conn->prepare($sql);
+        $stmn->execute();
+        $datos= $stmn->fetchAll(PDO::FETCH_ASSOC);
+        return $datos;
+    }
+    public function crear($matricula,$marca,$color,$kilometros){
+        if($this->leerMatricula($matricula)){
+         echo "Ya existe un vehiculo con matricula = $matricula";
+         return false;
+        }else{
+             $sql="INSERT INTO vehiculos (matricula,marca,color, kilometros) VALUES ('$matricula','$marca','$color',$kilometros)";
+             $stmn=$this ->conn->prepare($sql);
+         if( $stmn->execute()){
+             echo "creado correctamente";
+             return true;
+         }else{
+             echo "error al crear";
+             return false;
+         }
+        }
+     }
+     public function eliminar($id){
+        if($this->leer($id)){
+            $sql = "DELETE FROM vehiculos where id = $id";
+            $stmt = $this->conn->prepare($sql);
+            if($stmt->execute()){
+                echo "Eliminado correctamente";
+                return true;
+            }else{
+                echo "No Eliminado";
+                return false;
+            }
+        }else{
+            echo "No existe un vehiculo con id = $id";
+            return false;
+        }
+    }
+    public function actualizar($matricula,$marca,$color,$kilometros,$id){
+
+        if($this->leer($id)){
+        $sql="UPDATE vehiculos set matricula='$matricula',marca='$marca',color='$color',kilometros=$kilometros where id=$id";
+        $stmn=$this ->conn->prepare($sql);
+        if( $stmn->execute()){
+            echo "Actualizado Correctamente";
+         return true;
+        }else{
+            echo "Error al actualizar";
+         return false;
+        }
+        }else{
+            echo "No existe un vehiculo con id = $id";
+            return false;
+        }
+    }
+}
+$matricula='6789XYZ';
+$marca ='YAMAHA R1';
+$color='AMARILLO';
+$kilometros=12000;
+
+
+
+$u= new Usuario();
+var_dump($u->actualizar($matricula,$marca,$color,$kilometros,12));
+
 ?>
